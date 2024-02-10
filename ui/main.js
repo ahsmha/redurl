ec99797602b4b298b6fcc3eead297d9e464b3c11
ahsmha <ahmed.5wna0@aleeas.com>
2024-02-10 12:18:03 +0530

    add basic ui

---
+++ ui/main.js
@@ -0,0 +1,55 @@
+const saveBtn = document.getElementById('save');
+
+saveBtn.addEventListener('click', () => {
+    let url = document.getElementById('url').value.trim();
+    let redurl = document.getElementById('redirecturl').value.trim();
+
+    if (url && redurl) {
+        // add validation if urls are correct formatted
+        saveEntry(url, redurl);
+    } else {
+        alert('please fill in both url fields');
+    }
+});
+
+function saveEntry(url, redurl) {
+    browser.storage.local.get('redirects').then((result) => {
+        let redirects = result.redirects || {};
+        redirects[url] = redurl;
+        browser.storage.local.set({ 'redirects': redirects });
+    });
+}
+
+// displaying the extension storage for redirects
+
+function displayRules(redirects) {
+    const tbody = document.getElementsByTagName('tbody');
+    tbody.innerHTML = '';
+
+    // get redirects
+    for (let [url, redurl] of Object.entries(redirects)) {
+        let row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${url}</td>
+            <td>${redurl}</td>
+            <td><button onclick="removeRow(this, '${url}')">remove</button></td>
+        `;
+        tbody.appendChild(row);
+    }
+}
+
+function removeRule(button, url) {
+    let row = button.parentNode;
+    row.parentNode.removeChild(row);
+    // remove from local storage as well
+    browser.storage.local.get('redirects').then((result) => {
+        let redirects = result.redirects || {};
+        delete redirects[url];
+        browser.storage.local.set({'redirects': redirects })
+    });
+}
+
+browser.storage.local.get('redirects').then((result) => {
+    let redirects = result.redirects || {};
+    displayRules(redirects);
+})
